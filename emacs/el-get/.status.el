d2487f53a79fb89cc0d1d86f84941466037dd62f
Billy He <qiushihe@me.com>
2014-11-26 00:32:37 -0800

    Add el-get and markdown-mode

---
+++ emacs/el-get/.status.el
@@ -0,0 +1,8 @@
+((el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
+		("el-get.*\\.el$" "methods/")
+		:load "el-get.el"))
+ (markdown-mode status "installed" recipe
+		(:name markdown-mode :description "Major mode to edit Markdown files in Emacs" :website "http://jblevins.org/projects/markdown-mode/" :type git :url "git://jblevins.org/git/markdown-mode.git" :prepare
+		       (add-to-list 'auto-mode-alist
+				    '("\\.\\(md\\|mdown\\|markdown\\)\\'" . markdown-mode)))))
--- emacs/init.el
+++ emacs/init.el
@@ -28,6 +28,17 @@ inhibit-startup-echo-area-message t)
 (when (display-graphic-p)
   (global-linum-mode 1))
 
+;; Setup el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
