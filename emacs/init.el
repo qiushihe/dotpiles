654ad7c19deff3056d77e24c3e6e601345a7a156
Billy He <qiushihe@me.com>
2014-11-27 19:25:50 -0800

    Tidy up some code

--- emacs/init.el
+++ emacs/init.el
@@ -66,7 +66,12 @@ inhibit-startup-echo-area-message t)
 ;; Line number
 (global-linum-mode 1)
 (unless (display-graphic-p) ; Only add space between line number and text in non-GUI mode
-  (setq linum-format (lambda (line) (propertize (format (let ((w (length (number-to-string (count-lines (point-min) (point-max)))))) (concat "%" (number-to-string w) "d ")) line) 'face 'linum))))
+  (setq linum-format (lambda (line)
+    (propertize
+      (format
+        (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+          (concat "%" (number-to-string w) "d "))
+        line) 'face 'linum))))
 
 ;; Thin cursor
 (defun cursor-shape-hook ()
