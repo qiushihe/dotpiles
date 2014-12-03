d90e4dc90c420328cd19a73db868037681f94631
Billy He <qiushihe@me.com>
2014-12-02 22:38:28 -0800

    Add some test agenda files

--- emacs/init.el
+++ emacs/init.el
@@ -184,3 +184,16 @@ inhibit-startup-echo-area-message t)
 ;; Enable disabled commands
 (put 'scroll-left 'disabled nil)
 (put 'scroll-right 'disabled nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/OrgMode/Bouvier.Graham.org" "~/Dropbox/OrgMode/test.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
