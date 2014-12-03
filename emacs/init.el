bae6e5301b8352ed9eb26a0ec147561023214444
Billy He <qiushihe@me.com>
2014-12-03 13:46:26 -0800

    Update org mode file list

--- emacs/init.el
+++ emacs/init.el
@@ -190,7 +190,9 @@ inhibit-startup-echo-area-message t)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/OrgMode/Bouvier.Graham.org" "~/Dropbox/OrgMode/test.org"))))
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/OrgMode/MetaLab.org" "~/Dropbox/OrgMode/Personal.org" "~/Dropbox/OrgMode/test.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
