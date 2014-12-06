d344394c64c59019604aea59794c56847f0ded9d
Billy He <qiushihe@me.com>
2014-12-06 00:22:56 -0800

    Add binding for cursor movement keys

--- emacs/init.el
+++ emacs/init.el
@@ -92,6 +92,12 @@ inhibit-startup-echo-area-message t)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
+; Cursor movement
+(global-set-key (kbd "s-<left>") 'move-beginning-of-line)
+(global-set-key (kbd "s-<right>") 'move-end-of-line)
+(global-set-key (kbd "s-<up>") 'beginning-of-buffer)
+(global-set-key (kbd "s-<down>") 'end-of-buffer)
+
 ;; Keyboard scrolling speed
 (setq scroll-step 10)
 (setq hscroll-step 10)
