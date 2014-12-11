fa062c09015b9a224270b6ed025f1df8d588d283
Billy He <qiushihe@me.com>
2014-12-10 20:06:53 -0800

    Fix an issue tab key normalization and Makefile

--- emacs/user-packages/fix-enter-tab.el
+++ emacs/user-packages/fix-enter-tab.el
@@ -1,14 +1,32 @@
 ;; Make tab behave "normally"
-(defun my-tab ()
+(defun my-tab-with-space ()
   (interactive)
-  (insert "  "))
-(global-set-key "\t" 'my-tab)
-(add-hook 'after-change-major-mode-hook (lambda () (local-set-key "\t" 'my-tab)))
+  (insert "  ")
+)
+
+(defun my-tab-with-tab ()
+  (interactive)
+  (insert "\t")
+)
+
+(global-set-key "\t" 'my-tab-with-space)
+(add-hook 'text-mode-hook (lambda ()
+  (local-set-key "\t" 'my-tab-with-space)
+))
+
+;; But for makefile-mode use \t instead of space
+(add-hook 'makefile-mode-hook (lambda ()
+  (local-set-key "\t" 'my-tab-with-tab)
+))
 
 ;; Make auto-indent behave "normally"
 (defun my-enter ()
   (interactive)
   (newline)
-  (indent-relative-maybe))
+  (indent-relative-maybe)
+)
+
 (global-set-key (kbd "RET") 'my-enter)
-(add-hook 'after-change-major-mode-hook (lambda () (local-set-key (kbd "RET") 'my-enter)))
+(add-hook 'text-mode-hook (lambda ()
+  (local-set-key (kbd "RET") 'my-enter)
+))
