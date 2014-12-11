df7092199d788fab88208901b5bcf8a0caac86a3
Billy He <qiushihe@me.com>
2014-12-10 19:11:25 -0800

    Fix a calculation error with indent/outdent region end point

--- emacs/user-packages/fix-indent-outdent.el
+++ emacs/user-packages/fix-indent-outdent.el
@@ -3,31 +3,55 @@
 (defun my-normalize-region (rs re fn)
   (goto-char rs)
   (if (> rs (line-beginning-position))
-      (my-normalize-region (line-beginning-position) re fn)
-      (and (goto-char re)
-           (if (< re (line-end-position))
-               (my-normalize-region rs (line-end-position) fn)
-               (funcall fn rs re)))))
+    (my-normalize-region (line-beginning-position) re fn)
+    (progn
+      (goto-char re)
+      (if (< re (line-end-position))
+        (my-normalize-region rs (line-end-position) fn)
+        (funcall fn rs re)
+      )
+    )
+  )
+)
 
 (defun my-region-outdent (rs re)
   (goto-char rs)
-  (while (<= (point) re)
-         (goto-char (line-beginning-position))
-         (if (re-search-forward "^  " (line-end-position) t)
-             (replace-match "" nil t)
-             (if (re-search-forward "^ " (line-end-position) t)
-                 (replace-match "" nil t)))
-         (next-line 1)))
+  (if (<= (point) re)
+    (progn
+      (goto-char (line-beginning-position))
+      (if (re-search-forward "^  " (line-end-position) t)
+        (progn
+          (replace-match "" nil t)
+          (next-line 1)
+          (my-region-outdent (point) (- re 2))
+        )
+        (if (re-search-forward "^ " (line-end-position) t)
+          (progn
+            (replace-match "" nil t)
+            (next-line 1)
+            (my-region-outdent (point) (- re 1))
+          )
+          (progn
+            (next-line 1)
+            (my-region-outdent (point) re)
+          )
+        )
+      )
+    )
+  )
+)
 
-;; TODO: This function has a bug in that if the selection doesn't go beyond the end if line for the
-;;       last line in selection, the two spaces wont be insert to the last line. However if the two
-;;       spaces were replaced with some other characters (i.e. "++") then they would be inserted.
 (defun my-region-indent (rs re)
   (goto-char rs)
-  (while (<= (point) re)
-         (goto-char (line-beginning-position))
-         (insert "  ")
-         (next-line 1)))
+  (if (<= (point) re)
+    (progn
+      (goto-char (line-beginning-position))
+      (insert "  ")
+      (next-line 1)
+      (my-region-indent (point) (+ re 2))
+    )
+  )
+)
 
 ;; TODO: This function (as well as 'my-outdent-region) needs to be smarter about deactivating the
 ;;       selection. Currently deactivate-mark is set to nil so that repeated (in/out)dent can be
@@ -38,15 +62,19 @@
   (interactive)
   (save-excursion
 	  (if (< (point) (mark)) (exchange-point-and-mark))
-    (my-normalize-region (mark) (point) 'my-region-outdent))
-  (setq deactivate-mark nil))
+    (my-normalize-region (mark) (point) 'my-region-outdent)
+  )
+  (setq deactivate-mark nil)
+)
 
 (defun my-indent-region ()
   (interactive)
   (save-excursion
     (if (< (point) (mark)) (exchange-point-and-mark))
-    (my-normalize-region (mark) (point) 'my-region-indent))
-  (setq deactivate-mark nil))
+    (my-normalize-region (mark) (point) 'my-region-indent)
+  )
+  (setq deactivate-mark nil)
+)
 
 (global-set-key (kbd "C-{") 'my-outdent-region)
 (global-set-key (kbd "C-}") 'my-indent-region)
