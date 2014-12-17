fd2a3d03d19ecc5f4ce85591aaf433b0a9849896
Billy He <qiushihe@me.com>
2014-12-17 09:31:11 -0800

    Update line number format

--- emacs/user-packages/line-number.el
+++ emacs/user-packages/line-number.el
@@ -1,9 +1,24 @@
 ;; Line number
 (global-linum-mode 1)
-(unless (display-graphic-p) ; Only add space between line number and text in non-GUI mode
-  (setq linum-format (lambda (line)
-    (propertize
-      (format
-        (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
-          (concat "%" (number-to-string w) "d "))
-        line) 'face 'linum))))
+
+(if (display-graphic-p)
+  ;; For GUI, only add a " " before the number since there is by default a " " after
+  ;; the number already
+  (progn
+    (setq linum-format (lambda (line)
+      (propertize
+        (format
+          (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+            (concat " %" (number-to-string w) "d"))
+          line) 'face 'linum)))
+  )
+  ;; For non-GUI, add a " " before and after the number
+  (progn
+    (setq linum-format (lambda (line)
+      (propertize
+        (format
+          (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+            (concat " %" (number-to-string w) "d "))
+          line) 'face 'linum)))
+  )
+)
