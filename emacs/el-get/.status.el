8a239a066466e829d99297de1a54b7da84105ff3
Billy He <qiushihe@me.com>
2014-12-26 23:16:47 -0800

    Update packages

--- emacs/el-get/.status.el
+++ emacs/el-get/.status.el
@@ -18,7 +18,25 @@
  (el-get status "installed" recipe
          (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
                 ("el-get.*\\.el$" "methods/")
-                :load "el-get.el"))
+                :load "el-get.el" :post-init
+                (when
+                    (memq 'el-get
+                          (bound-and-true-p package-activated-list))
+                  (message "Deleting melpa bootstrap el-get")
+                  (unless package--initialized
+                    (package-initialize t))
+                  (when
+                      (package-installed-p 'el-get)
+                    (let
+                        ((feats
+                          (delete-dups
+                           (el-get-package-features
+                            (el-get-elpa-package-directory 'el-get)))))
+                      (el-get-elpa-delete-package 'el-get)
+                      (dolist
+                          (feat feats)
+                        (unload-feature feat t))))
+                  (require 'el-get))))
  (epl status "installed" recipe
       (:name epl :description "EPL provides a convenient high-level API for various package.el versions, and aims to overcome its most striking idiocies." :type github :pkgname "cask/epl"))
  (expand-region status "installed" recipe
