24d4fcd328e801a9c1d0d1fc81a62299734da2fc
Billy He <qiushihe@me.com>
2015-01-05 15:20:23 -0800

    Fix an issue with handlebars mode key binding conflict

--- emacs/el-get-user/init-files/init-ag.el
+++ emacs/el-get-user/init-files/init-ag.el
@@ -2,3 +2,8 @@
 (setq ag-reuse-window t)
 
 (global-set-key (kbd "C-c s p") 'ag-project)
+
+;; Remove a conflicting key binding in handlebars mode
+(add-hook `handlebars-mode-hook (lambda ()
+  (local-unset-key (kbd "C-c s"))
+))
