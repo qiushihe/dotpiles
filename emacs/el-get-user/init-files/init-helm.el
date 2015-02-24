3d43ca0c9bdad577d4a79535a0151c94623e0ada
Billy He <qiushihe@me.com>
2015-02-24 14:30:22 -0800

    Update helm settings

--- emacs/el-get-user/init-files/init-helm.el
+++ emacs/el-get-user/init-files/init-helm.el
@@ -1,11 +1,16 @@
+;; Enable helm
 (helm-mode 1)
+
+;; Setup helm key bindings/overrides
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-mini)
-
 (global-set-key (kbd "C-x f") 'helm-find-files)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-
 (global-set-key (kbd "C-c s o") 'helm-occur)
+
+;; Create helm split inside the current window instead of seemingly randomly
+;; taking over a different window
+(setq helm-split-window-in-side-p t)
 
 ;; Override helm fuzzy matching function to disable the feature
 (defun helm-fuzzy-match (candidate))
