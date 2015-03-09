ecc210f49a0c7adef5420b5a9972517e04102c5f
Billy He <qiushihe@me.com>
2015-03-08 19:10:04 -0700

    Update some orgmode settings

--- emacs/el-get-user/init-files/init-org-mode.el
+++ emacs/el-get-user/init-files/init-org-mode.el
@@ -1,3 +1,15 @@
 (setq org-support-shift-select t)
 
 (add-to-list 'auto-mode-alist '("\\.org.txt\\'" . org-mode))
+
+(setq org-directory "~/Dropbox/Org")
+
+;; Setup capture
+(setq org-default-notes-file (concat org-directory "/Inbox.org"))
+(define-key global-map (kbd "C-c o c") 'org-capture)
+
+;; Setup agenda files
+(setq org-agenda-files (list
+  (concat org-directory "/Inbox.org")
+  (concat org-directory "/test.org")
+))
--- emacs/init.el
+++ emacs/init.el
@@ -105,9 +105,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Dropbox/Org/index.org" "~/Dropbox/Org/MetaLab.org" "~/Dropbox/Org/Walmart.org"))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
