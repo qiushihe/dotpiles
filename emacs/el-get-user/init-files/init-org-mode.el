166c7683ae86d400ce3e06250c3253f3e3d9f3ce
Billy He <qiushihe@me.com>
2015-03-13 15:50:18 -0700

    Update org-mode templates

--- emacs/el-get-user/init-files/init-org-mode.el
+++ emacs/el-get-user/init-files/init-org-mode.el
@@ -7,6 +7,10 @@
 ;; Setup capture
 (setq org-default-notes-file (concat org-directory "/Inbox.org"))
 (define-key global-map (kbd "C-c o c") 'org-capture)
+(setq org-capture-templates '(
+  ("t" "Todo" entry (file+headline org-default-notes-file "Tasks") "* TODO %?")
+  ("n" "Note" entry (file+headline org-default-notes-file "Notes") "* %U %?")
+))
 
 ;; Setup agenda files
 (setq org-agenda-files (list
