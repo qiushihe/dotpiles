91cd89fd9029c9d9c968fb104f6c84eb4ece158c
Billy He <qiushihe@me.com>
2015-04-08 09:04:59 -0700

    Ensure after-make-frame hook updates the face of the correct frame

--- emacs/init.el
+++ emacs/init.el
@@ -120,8 +120,8 @@
 (add-hook 'after-make-frame-functions
   (lambda (frame)
     (select-frame frame)
-    (set-face-attribute 'fringe nil :background (face-attribute 'default :background) :foreground (face-attribute 'fringe :foreground))
-    (set-face-attribute 'linum nil :background (face-attribute 'default :background) :foreground (face-attribute 'linum :foreground) :slant 'normal)
+    (set-face-attribute 'fringe frame :background (face-attribute 'default :background) :foreground (face-attribute 'fringe :foreground))
+    (set-face-attribute 'linum frame :background (face-attribute 'default :background) :foreground (face-attribute 'linum :foreground) :slant 'normal)
   )
 )
 
