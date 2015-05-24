47b2a1a06629dccb31f54e4f1d4fa5f023c23270
Billy He <qiushihe@me.com>
2015-05-23 18:42:43 -0700

    Add projectile root advice to short circuit working-directory value

--- emacs/el-get-user/init-files/init-projectile.el
+++ emacs/el-get-user/init-files/init-projectile.el
@@ -1,2 +1,19 @@
 (projectile-global-mode)
 (setq projectile-enable-caching t)
+
+;; Advice projectile-root-bottom-up to short circuit and return the value
+;; of 'working-directory frame variable, defined by the emacsl script.
+;; This gets around the issue of projectile incorrectly assuming submodule
+;; root to be project root when inside a submodule in a project.
+;; This method of advising projectile-root-bottom-up allows dynamic project
+;; detection without having to create the .projectile file.
+(defun my-projectile-root-bottom-up (orig-fun &rest args)
+  (if (boundp 'working-directory)
+    (if (string= (substring working-directory -1) "/")
+      working-directory
+      (concat working-directory "/")
+    )
+    (apply orig-fun args)
+  )
+)
+(advice-add 'projectile-root-bottom-up :around #'my-projectile-root-bottom-up)
